8cd2690c201974e72773ee67f0008559393fe5d7
Daniel Millikan <circleci.roar@gmail.com>
2022-02-03 21:41:51 -0600

    fix it

--- .circleci/git-pr-merge.sh
+++ .circleci/git-pr-merge.sh
@@ -7,9 +7,10 @@ then
     IFS=$' \t\n'
     URL="https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pulls/${PULL_REQUEST_NUMBER}"
     BASE=$(curl -s -X GET -H "Accept: application/vnd.github.v3+json"  ${URL} | jq -r .base.ref)
-    git checkout $BASE
+    # git checkout ${BASE}
+    # git checkout ${CIRCLE_BRANCH}
 fi
 
 git config --global user.email "circleci-pre-merge@circleci.roar+pr${PULL_REQUEST_NUMBER}@gmail.com"
 git config --global user.name "CircleCI Pre Merge PR # ${PULL_REQUEST_NUMBER}"
-git merge ${base} --no-commit
+git merge ${BASE} --no-commit
